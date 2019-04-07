73d63e5cb2887086061211fe84caa0da502c50ea
mingfei <mingfei@arblabs.com>
2019-04-07 00:59:49 -0400

    bug fix

--- src/java/org/apache/cassandra/dht/Murmur3Partitioner.java
+++ src/java/org/apache/cassandra/dht/Murmur3Partitioner.java
@@ -232,7 +232,7 @@ public class Murmur3Partitioner implements IPartitioner
     {
         long[] hash = new long[2];
         int start = key.position();
-        if ((key.get(start) & 0xaa) == 0 && (key.get(start + 3) & 0xaa) == 0) {
+        if (key.remaining() >= 4 && (key.get(start) & 0xaa) == 0 && (key.get(start + 3) & 0xaa) == 0) {
             logger.info("Found hint");
             MurmurHash.hash3_x64_128(key, key.position() + 4, key.remaining(), 0, hash);
         }
